fdb231df9a3c93e228029aa7486aa3be041b64a8
lucas vialle <lvialle87@gmail.com>
2026-09-07 17:24:28 -0300

    feat: cria estrutura inicial do menu

---
+++ main.js
@@ -0,0 +1,19 @@
+const readline = require( 'readline-sync')
+
+let sistemaRodando = true
+
+while (sistemaRodando) {
+    console.log("\n ===== Mini Erp ===== \n");
+    console.log("1 - Cadastrar Produto");
+    console.log("2 - Listar Produtos");
+    console.log("3 - Registrar Venda");
+    console.log("4 - Busca por Categoria");
+    console.log("5 - Relatório Geral");
+    console.log("0 - Sair do Sistema");
+
+    
+    let opcao = readline.question("Escolha uma opcao: ");
+
+
+
+}
