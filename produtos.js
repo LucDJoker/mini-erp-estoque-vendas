bffb9d06b3f41f1f616ae09a8c62a8e5eee2a14b
Lucas David <lvialle87@gmail.com>
2026-09-09 19:57:40 -0300

    Feat: completa crud de produtos

--- produtos.js
+++ produtos.js
@@ -17,4 +17,36 @@ function listarProdutos() {
         console.log("Quantidade em estoque: " + estoque[i].quantidade);
         console.log("------------------------");
     }
+
 }
+function buscarProduto(nome) {
+    const produtoEncontrado = estoque.find(produto => produto.nome.toLowerCase() === nome.toLowerCase());       
+    if (!produtoEncontrado) {
+        console.log("Produto não encontrado!");
+        return null;
+    }
+    console.log("Produto encontrado!");
+    return produtoEncontrado;   
+}
+function atualizarProduto(nome, novoNome, novaCategoria, novoPreco, novaQuantidade) {
+    const produtoEncontrado = estoque.find(produto => produto.nome.toLowerCase() === nome.toLowerCase());   
+    if (!produtoEncontrado) {
+        console.log("Produto não encontrado!");
+        return;
+    }
+    produtoEncontrado.nome = novoNome;
+    produtoEncontrado.categoria = novaCategoria;
+    produtoEncontrado.preco = novoPreco;
+    produtoEncontrado.quantidade = novaQuantidade;
+    console.log("Produto atualizado com sucesso!");
+}
+function removerProduto(nome) {
+    const index = estoque.findIndex(produto => produto.nome.toLowerCase() === nome.toLowerCase());
+    if (index === -1) {
+        console.log("Produto não encontrado!");
+        return;
+    }       
+    estoque.splice(index, 1);
+    console.log("Produto removido com sucesso!");
+}
+
