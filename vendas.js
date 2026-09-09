ab7db741da3d3a142f3b10528023887f83b67996
Yago Queiroz <yagoque15@gmail.com>
2026-09-09 18:44:28 -0300

    feat:atualizacao do estoque

--- vendas.js
+++ vendas.js
@@ -1,5 +1,5 @@
 function registroVenda (identificacaoProduto, quantidadeVendida, listaEstoque, listaVendas) {
-    if (identificacaoProduto || isNaN(quantidadeVendida) || (quantidadeVendida <= 0)) {
+    if (!identificacaoProduto || isNaN(quantidadeVendida) || (quantidadeVendida <= 0)) {
         return "Dados incorretos!.";
     }
     const produto = listaEstoque.find (p => p.id === identificacaoProduto);
@@ -15,8 +15,9 @@ const novaVenda ={
     produtoId: identificacaoProduto,
     quantidade: quantidadeVendida,
     valorTotal: quantidadeVendida * produto.preco,
+    data: new Date().toLocaleDateString()
 };
-lista.Vendas.push(novaVenda);
+listaVendas.push(novaVenda);
 return "Venda concluida com sucesso!";
 }
 
@@ -25,23 +26,44 @@ if(listaVendas.length === 0) {
     console.log ("Não existem vendas registradas!");
 return;
 }
+
 console.log ("Histórico de Vendas");
 listaVendas.forEach(venda => {
-    console.log("Identificacao da Venda: ${venda.idenfificacaoProduto}");
-    console.log("Data: ${venda.data}");
-    console.log("Identificacao do Produto: ${venda.identificacaoProduto}");
-    console.log("Quantidade: ${venda.quantidadeVendida}");
-    console.log("Valor Total: R$ ${venda.valorTotal}")
+    console.log(`Identificacao da Venda: ${venda.id}`);
+    console.log(`Data: ${venda.data}`);
+    console.log(`Identificacao do Produto: ${venda.id}`);
+    console.log(`Quantidade: ${venda.quantidade}`);
+    console.log(`Valor Total: R$ ${venda.valorTotal}`)
 });
 }
 
-function buscarVendaPorId(listaVendas, IdBuscar){
-if (idBuscar || isNan(idBuscar)){
-    return "Vanda não encontrada!";
+function buscarVenda(listaVendas, idBuscar){
+if (!idBuscar || isNaN(idBuscar)){
+    return "Venda não encontrada!";
 }
-const vendaEncontrada = listaVendas.find(venda => venda.identificacaoProduto === Number(idBuscar));
+const vendaEncontrada = listaVendas.find(venda => venda.id === Number(idBuscar));
 if (!vendaEncontrada){
     return "Venda não Encontrada!";
 }
 return vendaEncontrada;
+}
+
+function baixaEstoque(identificacaoProduto, quantidadeVendida, listaEstoque){
+if (!identificacaoProduto || isNaN(quantidadeVendida) || Number(quantidadeVendida <= 0)) {
+        console.log("ID incorreto ou quantidade incorreta!.");
+}
+}
+
+const produto = listaEstoque.find (produto => produto.id === Number(identificacaoProduto));
+if (!produto) {
+    console.log("Produto não encontrado")
+    return false;
+}
+if (produto.quantidade < Number(quantidadeVendida)) {
+    console.log ("Quantidade insuficiente em estoque");
+    return false;
+}
+produto.quantidade -= Number(quantidadeVendida);{
+    console.log("Estoque atualizado com sucesso")
+    return true
 }
