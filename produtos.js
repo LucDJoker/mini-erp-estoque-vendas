6ede303f9306b93e0b9846ad28d305e43e8b2843
Yago Queiroz <yagoque15@gmail.com>
2026-09-09 22:31:31 -0300

    feat: validacoes cadastrarProduto, listarProdutos e buscarProdutos feita

--- produtos.js
+++ produtos.js
@@ -1,26 +1,46 @@
+let estoque = [];
+
 function cadastrarProduto(nome, categoria, preco, quantidade) {
+    if (!nome || !categoria || isNaN(preco) || isNaN(quantidade) || preco <= 0 || quantidade < 0) {
+        console.log("Dados inválidos!");
+        return;
+    }
+    const produtoExistente = estoque.find(produto => produto.nome.toLowerCase() === nome.toString().toLowerCase());
+    if (produtoExistente) {
+        console.log("Produto Existente!");
+        return;
+    }
     estoque.push({ 
-        nome: nome, 
-        categoria: categoria, 
-        preco: preco, 
-        quantidade: quantidade 
+        nome: nome.toString(), 
+        categoria: categoria.toString(), 
+        preco: preco.toString(), 
+        quantidade: quantidade.toString() 
     });
     console.log("Produto cadastrado com sucesso!");
 }
 
 function listarProdutos() {
+    if (estoque.length ===0){
+        console.log("Não há produtos em estoque")
+        return;
+    }
+
     console.log("\n--- LISTA DE PRODUTOS ---");
     for (let i = 0; i < estoque.length; i++) {
         console.log("Nome: " + estoque[i].nome);
         console.log("Categoria: " + estoque[i].categoria);
-        console.log("Preco: R$" + estoque[i].preco);
+        console.log("Preco: R$" + estoque[i].preco.toFixed(2));
         console.log("Quantidade em estoque: " + estoque[i].quantidade);
         console.log("------------------------");
     }
 
 }
 function buscarProduto(nome) {
-    const produtoEncontrado = estoque.find(produto => produto.nome.toLowerCase() === nome.toLowerCase());       
+    if (!nome){
+        console.log ("Produto não encontrado")
+        return null;
+    }
+    const produtoEncontrado = estoque.find(produto => produto.nome.toLowerCase() === nome.toString().toLowerCase());       
     if (!produtoEncontrado) {
         console.log("Produto não encontrado!");
         return null;
